1fa2b3bece63896cb0e57567c9dea55c97039d5a
adriana.nogueira <adriana.nogueira@zup.com.br>
2024-12-02 17:38:37 -0300

    Create Variable

---
+++ DeclarandoVariaveis.java
@@ -0,0 +1,17 @@
+public class DeclarandoVariaveis {
+
+    String nome = "Adriana Borges Nogueira";
+    int idade = 20;
+    double altura = 1.70;
+    boolean matricula = true;
+
+
+    public static void main(String[] args) {
+       DeclarandoVariaveis declarandoVariaveis = new DeclarandoVariaveis();
+       System.out.println("Nome: " + declarandoVariaveis.nome);
+       System.out.println("Idade: " + declarandoVariaveis.idade);
+       System.out.println("Altura: " + declarandoVariaveis.altura);
+       System.out.println("Matricula: " + declarandoVariaveis.matricula);
+
+    }
+}
